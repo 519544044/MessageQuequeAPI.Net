2c81c8c960c8728f5bb0e19594ff7ffa600d3ce3
hyc <519544044@qq.com>
2017-07-28 17:24:04 +0800

    调整文件夹

---
+++ MessageQuequeAPI.Kafka/KafkaConsumerClientFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace DotNetCore.CAP.Kafka
+{
+    public class KafkaConsumerClientFactory : IConsumerClientFactory
+    {
+        private readonly KafkaOptions _kafkaOptions;
+
+        public KafkaConsumerClientFactory(IOptions<KafkaOptions> kafkaOptions)
+        {
+            _kafkaOptions = kafkaOptions.Value;
+        }
+
+        public IConsumerClient Create(string groupId)
+        {
+            return new KafkaConsumerClient(groupId, _kafkaOptions);
+        }
+    }
+}
---
+++ MessageQuequeAPI.Kafka/MyQueque.KafkaOptions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageQuequeAPI
+{
+    public class KafkaOptions
+    {
+        public KafkaOptions()
+        {
+            MainConfig = new Dictionary<string, object>();
+        }
+
+        /// <summary>
+        /// librdkafka configuration parameters (refer to https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md).
+        /// <para>
+        /// Topic configuration parameters are specified via the "default.topic.config" sub-dictionary config parameter.
+        /// </para>
+        /// </summary>
+        public IDictionary<string, object> MainConfig { get; private set; }
+
+        /// <summary>
+        /// The `bootstrap.servers` item config of `MainConfig`.
+        /// <para>
+        /// Initial list of brokers as a CSV list of broker host or host:port.
+        /// </para>
+        /// </summary>
+        public string Servers { get; set; }
+
+        internal IEnumerable<KeyValuePair<string, object>> AsRdkafkaConfig()
+        {
+            if (MainConfig.ContainsKey("bootstrap.servers"))
+                return MainConfig.AsEnumerable();
+
+            if (string.IsNullOrEmpty(Servers))
+            {
+                throw new ArgumentNullException(nameof(Servers));
+            }
+            else
+            {
+                MainConfig.Add("bootstrap.servers", Servers);
+            }
+            MainConfig["enable.auto.commit"] = "false";
+            return MainConfig.AsEnumerable();
+        }
+    }
+}
